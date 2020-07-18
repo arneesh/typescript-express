fa26853c98eb4b59dbb354adfff738f723d34a10
arneesh <arneeshaima@gmail.com>
2020-07-18 12:02:27 +0530

    added session functionality

---
+++ src/routes/loginRoutes.ts
@@ -0,0 +1,70 @@
+import { Router, Request, Response, request } from 'express';
+
+interface RequestWithBody extends Request {
+    body: { [key:string]: string | undefined }
+}
+
+
+const router = Router();
+
+router.get('/login', (req: Request,res: Response) => {
+    res.send(`
+    <form method="POST">
+    <div>
+    <label> Email </label>
+    <input name="email" />
+    </div>
+    <div>
+    <label> Password </label>
+    <input name="password" type="password" />
+    </div>
+    <button> Sumbit </button>
+    </form>
+    `);
+});
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const { email, password } = req.body;
+
+
+    // console.log("Request", req.body);
+
+    if(email && password && (email ==='arn@gmail.com') && (password==='abc')){
+
+        req.session = {loggedIn: true};
+        res.redirect('/');
+
+    } else {
+        res.send('Email Property is required');
+    }
+
+});
+
+router.get('/', (req: Request , res: Response ) => {
+
+
+    if( req.session && req.session.loggedIn){
+
+        res.send(`
+            <div>
+            <div> You are logged in</div>
+            <a href="/logout"> Logout </a>
+            </div>
+        `);
+    } else {
+        res.send(`
+            <div>
+            <div> You are not logged in</div>
+            <a href="/login"> Login </a>
+            </div>
+        `);
+    }
+});
+
+router.get('/logout', (req: Request, res: Response) => {
+    req.session = null;
+    res.redirect('/');
+});
+
+
+export { router };
